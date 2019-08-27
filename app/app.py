22d4d7a1fdced4f099b6024d1b080ae342b2ff1e
Kelsey Campbell <campkels@gmail.com>
2019-08-26 20:17:38 -0400

    File upload - local, maybe

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 # Thx https://medium.com/fintechexplained/flask-host-your-python-machine-learning-model-on-web-b598151886d
 # Make sure VM is activated - bash workon my-virtualenv (https://help.pythonanywhere.com/pages/Flask/)
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+import os
 import re
 import nltk
 import pickle
@@ -41,7 +42,7 @@ model = pickle.load(open('finalized_model.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
-def show_predict_stock_form():
+def show_predict_form():
     return render_template('predictorform.html')
 @app.route('/result', methods=['POST'])
 def results():
@@ -56,6 +57,14 @@ def results():
       predicted_label = originallabels[predicted[0]]
       print(predicted)
       return render_template('resultsform.html', text=rawtext,   predicted_author=predicted_label)
+
+@app.route("/handleUpload", methods=['POST'])
+def handleFileUpload():
+	if 'csv' in request.files:
+		csv = request.files['csv']
+		if csv.filename != '':
+			csv.save(os.path.join('C:/Users/Kelsey/Desktop/jobs/TheTrevorProject/Project/app/uploads', csv.filename))
+	return redirect(url_for('handleFileUpload'))
 
 # Comment this out when deploy
 app.run(port=5000, debug=True)
